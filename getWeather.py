08d33c1f39b6463f4a3d53d93493f89225d75cf4
NANA-GALORE <nana-galore@ubuntu.ubuntu-domain>
2012-07-04 11:48:56 +0000

    COmmitting Lab_08-RegEx

--- getWeather.py
+++ getWeather.py
@@ -5,4 +5,9 @@ import re
 f = urllib.urlopen("http://www.wunderground.com/global/stations/65432.html")
 # Read from the object, storing the page's contents in 's'.
 s = f.read()
+match=re.search(r'<div id="tempActual">.*?>(<span\sclass="b">)(\d*?)<',s,re.DOTALL)
+if match!=None:
+    print 'THE TEMPERATURE NOW IS', match.group(2),'°C'
+else:
+    print 'Nothing yet'
 f.close()
--- reExamples.py
+++ reExamples.py
@@ -21,3 +21,47 @@ def getAdjs(inp):
     #return re.findall(r'[a-zA-Z0-9_]+y', inp) # the same thing
     #return re.findall(r'\w{1,}y', inp) # the same thing
 print getAdjs('the funny book was goofy')
+
+
+
+##
+print '\n\n NUMBER 3'
+print '**************'
+
+
+def isEmail(inp):
+    return re.match('(\w)+@\w+(\.\w)+',inp) != None
+
+print isEmail('sd$sd@hello.com')
+##  nanab@aiti-kace.com.gh
+##  nanagalore@aol.com
+##  nana12345@yahoo.com
+##  nana_smiles@ovi.com
+##  sd$sd@hello.com
+
+
+
+##
+print '\n\n NUMBER 4'
+print '**************'
+
+
+def getTxts(files):
+    return re.findall(r'\w+\.txt',files)
+
+print getTxts('yo.html blah.txt woah.txt he ')
+
+
+
+print '\n\n NUMBER 5'
+print '**************'
+
+def percAwesome(inp):
+    num = float(len(re.findall('(\w)+',inp))) #finding number of words
+    aw_num = float(len(re.findall('awes[o|0]me',inp))) #finding number of 'awes[o|0]me'
+    return round(((aw_num/num)*100),1)#returning percentage of awes[o|0]me
+
+print percAwesome('iamawesomeblah and awes0me is as awesomeo does')
+
+## iamawesomeblah and awes0me is as awesomeo does
+## hello my name is wayawesomedude
